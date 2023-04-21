99ce0061486ab38f0fc0a3dcc042d72d066f98bf
seunghee <kanada11416@gmail.com>
2023-04-21 11:05:11 +0900

    add : querydsl setting

---
+++ src/main/java/com/study/querydsl/entitly/Hello.java
@@ -0,0 +1,17 @@
+package com.study.querydsl.entitly;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Hello {
+    @Id @GeneratedValue
+    private Long id;
+}
