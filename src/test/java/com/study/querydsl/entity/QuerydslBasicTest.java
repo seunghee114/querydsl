ab6a55b32075c481adcabcb53fa12b1e4ee012cd
seunghee <kanada11416@gmail.com>
2023-04-25 00:58:58 +0900

    add : querydsl join

--- src/test/java/com/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/entity/QuerydslBasicTest.java
@@ -193,4 +193,78 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 
+    /**
+     * 팀 A에 소속된 모든 회원
+     * */
+    @Test
+    public void join(){
+        List<Member> result = queryFactory.selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타조인 (연관관계가 없는 필드로 조인할 수 있음)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     * 모든 멤버 테이블이랑 모든 팀 테이블을 조인 시켜서 팀이름이랑 멤버이름이 같은 거 가져오기
+     * */
+
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory.select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+    /**
+     * 회원과 팀을 조인하면서,
+     * 팀 이름이 teamA인 팀만 조인
+     * 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     * */
+    @Test
+    public void join_on_filtering(){
+        List<Tuple> result = queryFactory.select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+        for(Tuple tuple : result){
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 회원 외부조인
+     * pk, fk 값 매칭이 없음
+     * */
+
+    @Test
+    public void join_on_no_relation(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory.select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for(Tuple tuple : result){
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 }
