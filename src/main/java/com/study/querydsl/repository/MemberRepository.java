eb7d429c897380b05c591c35b90ac0a5b4fc55e8
seunghee <kanada11416@gmail.com>
2023-08-09 11:30:42 +0900

    add 사용자 정의 repository

--- src/main/java/com/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom{
+                                    // 인터페이스 다중 상속 가능
     List<Member> findByUsername(String username);
 }
--- src/test/java/com/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.study.querydsl.repository;
 
+import com.study.querydsl.dto.MemberSearchCondition;
+import com.study.querydsl.dto.MemberTeamDto;
 import com.study.querydsl.entitly.Member;
+import com.study.querydsl.entitly.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +32,30 @@ public class MemberRepositoryTest {
         assertThat(result1).containsExactly(member);
         List<Member> result = memberRepository.findByUsername("seunghee");
         assertThat(result).containsExactly(member);
+    }
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        // 조건이 아예 없을 경우에 쿼리가 데이터를 다 끌어온다. -> 데이터가 많을 경우 문제가 된다.
+        condition.setTeamName("teamB");
+        List<MemberTeamDto> result = memberRepository.search(condition);
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
 }
